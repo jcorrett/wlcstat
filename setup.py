7af111b0a4f1e107ecf6fcc01010473be1f1d418
jcorrett <70544282+jcorrett@users.noreply.github.com>
2022-08-02 20:25:55 -0700

    Added temp setup.py file for pip installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='wlcstat',
+    version='',
+    packages=['wlcstat', 'wlcstat.util'],
+    url='',
+    license='',
+    author='Andrew Spakowitz',
+    author_email='',
+    description=''
+)
